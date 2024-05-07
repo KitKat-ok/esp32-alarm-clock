42d99de0e9c0bc4537eb385aceedd7ff9f55d050
KitKat-ok <Pascalsch3@gmail.com>
2024-05-07 15:22:02 +0200

    added confidential

---
+++ src/confidential.h
@@ -0,0 +1,20 @@
+#ifndef CONFIDENTIAL_H
+#define CONFIDENTIAL_H
+
+// Wifi
+#define SSID1 ""
+#define PASSWORD1 ""
+#define SSID2 ""
+#define PASSWORD2 ""
+
+// Weather
+#define OPEN_WEATHER_API_KEY ""
+#define WEATHER_LATIT ""
+#define WEATHER_LONGTIT ""
+#define WEATHER_UNIT "metric"
+#define WEATHER_LANG "en"
+
+#define WEATHER_PER_DAY 8
+
+
+#endif
