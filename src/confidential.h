407b61a963d6fa3876af06b80f5c16a3df8a3362
KitKat-ok <Pascalsch3@gmail.com>
2024-07-25 16:05:38 +0200

    changed confidential file

--- src/confidential.h
+++ src/confidential.h
@@ -2,19 +2,15 @@
 #define CONFIDENTIAL_H
 
 // Wifi
-#define SSID1 ""
-#define PASSWORD1 ""
-#define SSID2 ""
-#define PASSWORD2 ""
+#define SSID1 
+#define PASSWORD1 
+#define SSID2
+#define PASSWORD2
+#define SSID3 
+#define PASSWORD3 
 
 // Weather
-#define OPEN_WEATHER_API_KEY ""
-#define WEATHER_LATIT ""
-#define WEATHER_LONGTIT ""
-#define WEATHER_UNIT "metric"
-#define WEATHER_LANG "en"
-
-#define WEATHER_PER_DAY 8
-
+#define WEATHER_LATIT 
+#define WEATHER_LONGTIT 
 
 #endif
